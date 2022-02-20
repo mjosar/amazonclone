42feaed3692e90830465698b72e935054a56e2d0
= <=>
2022-02-20 03:27:41 -0500

    upload

---
+++ src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBGRQ1nlvQGtMgcnF97MPom6mCaNTOwha0",
+    authDomain: "clone-50c88.firebaseapp.com",
+    projectId: "clone-50c88",
+    storageBucket: "clone-50c88.appspot.com",
+    messagingSenderId: "251593708451",
+    appId: "1:251593708451:web:61a193b6bcd5086aec8016",
+    measurementId: "G-4G5CZFGHEX"
+};
